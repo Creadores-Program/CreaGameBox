ab4ee3da28f86a091658b5817693cbadc5fe2af8
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-06-18 19:26:35 -0600

    Add MainActivity with WebView setup

---
+++ app/src/main/java/org/CreadoresProgram/CreaGameBox/MainActivity.java
@@ -0,0 +1,35 @@
+package org.CreadoresProgram.CreaGameBox;
+import org.CredoresProgram.WebViewCREA.WebViewCreaClient;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.graphics.Color;
+
+public class MainActivity extends Activity {
+  private WebView webview;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.layout_main);
+    WebView webView = (WebView) findViewById(R.id.webview);
+    webView.setWebViewClient(new WebViewCreaClient());
+    WebSettings webSettings = webView.getSettings();
+    webSettings.setJavaScriptEnabled(true);
+    webSettings.setDomStorageEnabled(true);
+    webSettings.setAllowFileAccess(true);
+    webSettings.setAllowContentAccess(true);
+    webSettings.setDatabaseEnabled(true);
+    webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+    webSettings.setBuiltInZoomControls(false);
+    webSettings.setDisplayZoomControls(false);
+    webSettings.setSupportZoom(false);
+    webSettings.setUseWideViewPort(true);
+    webSettings.setLoadWithOverviewMode(true);
+    webView.setBackgroundColor(Color.BLACK);
+    this.webview = webView;
+  }
+}
