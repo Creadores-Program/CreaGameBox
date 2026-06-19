e12c83a84de57890d27244d1c9a825ba14569e0e
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-06-19 13:53:00 -0600

    Refactor MainActivity to use WebViewHome
    Refactor MainActivity to initialize WebViewHome and add it to the layout.

--- app/src/main/java/org/CreadoresProgram/CreaGameBox/MainActivity.java
+++ app/src/main/java/org/CreadoresProgram/CreaGameBox/MainActivity.java
@@ -8,16 +8,24 @@ import android.webkit.WebView;
 import android.webkit.WebSettings;
 import android.webkit.WebViewClient;
 import android.graphics.Color;
+import android.widget.FrameLayout;
+
+import java.util.HashMap;
 
 public class MainActivity extends Activity {
-  private WebView webview;
+  private FrameLayout screenAndroid;
+  private WebView webviewHome;
+  private WebView webViewApp;
+  private HashMap<Integer, WebView> menuUsers = new HashMap<>();
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.layout_main);
-    WebView webView = (WebView) findViewById(R.id.webview);
-    webView.setWebViewClient(new WebViewClient());
-    WebSettings webSettings = webView.getSettings();
+    this.screenAndroid = findViewById(R.id.creagameboxScreen);
+    WebView webViewHome = new WebView(this);
+    webViewHome.setLayoutParams(new FrameLayout.LayoutParams(match_parent, match_parent));
+    webViewHome.setWebViewClient(new WebViewClient());
+    WebSettings webSettings = webViewHome.getSettings();
     webSettings.setJavaScriptEnabled(true);
     webSettings.setDomStorageEnabled(true);
     webSettings.setAllowFileAccess(true);
@@ -29,7 +37,16 @@ public class MainActivity extends Activity {
     webSettings.setSupportZoom(false);
     webSettings.setUseWideViewPort(true);
     webSettings.setLoadWithOverviewMode(true);
-    webView.setBackgroundColor(Color.BLACK);
-    this.webview = webView;
+    webSettings.setMediaPlaybackRequiresUserGesture(false);
+    webSettings.setAppCacheEnabled(true);
+    webSettings.setAppCachePath(getApplicationContext().getCacheDir().getAbsolutePath());
+    webSettings.setAllowFileAccessFromFileURLs(true);
+    webSettings.setAllowUniversalAccessFromFileURLs(true);
+    webViewHome.setLayerType(WebView.LAYER_TYPE_HARDWARE, null);
+    //add js interfaces
+    webViewHome.setBackgroundColor(Color.BLACK);
+    this.webviewHome = webViewHome;
+    //load Home url
+    screenAndroid.addView(webViewHome);
   }
 }
