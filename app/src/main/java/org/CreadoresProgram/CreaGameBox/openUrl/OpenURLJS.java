5eb726bf33dd83b5294d1dd64f61772a59b2bbbf
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-06-20 08:27:40 -0600

    Add OpenURLJS class for opening URLs in custom tabs

---
+++ app/src/main/java/org/CreadoresProgram/CreaGameBox/openUrl/OpenURLJS.java
@@ -0,0 +1,23 @@
+package org.CreadoresProgram.CreaGameBox.openUrl;
+
+import android.support.customtabs.CustomTabsIntent;
+import android.webkit.JavascriptInterface;
+import android.content.Context;
+import android.net.Uri;
+public class OpenURLJS{
+  private Context context;
+  public OpenURLJS(Context c){
+    this.context = c;
+  }
+  @JavascriptInterface
+  public void openUrl(String url){
+    CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+    builder.setShowTitle(true);
+    builder.setToolbarColor(Color.parseColor("#40E0D0"));
+    CustomTabsIntent customTabsIntent = builder.build();
+    if(url.startsWith("data:")){
+      url = "javascript:location.href=" + org.json.JSONObject.quote(url) + ";";
+    }
+    customTabsIntent.launchUrl(context, Uri.parse(url));
+  }
+}
