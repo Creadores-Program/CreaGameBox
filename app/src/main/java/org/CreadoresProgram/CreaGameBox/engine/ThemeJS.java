8e28cf72042cda017816d75ad50c78bb18fe5d46
Trollhunters501 <trollhunters500@outlook.com>
2026-06-24 22:13:51 -0600

    Añadir mas temas

--- app/src/main/java/org/CreadoresProgram/CreaGameBox/engine/ThemeJS.java
+++ app/src/main/java/org/CreadoresProgram/CreaGameBox/engine/ThemeJS.java
@@ -12,14 +12,58 @@ public class ThemeJS {
             case "base":
                 this.chclient.setTheme(android.R.style.Theme_Dialog);
                 break;
+            case "base_notitle":
+                this.chclient.setTheme(android.R.style.Theme_Dialog_NoTitleBar);
+                break;
+            case "base_alert":
+                this.chclient.setTheme(android.R.style.Theme_Dialog_Alert);
+                break;
             case "holo":
                 this.chclient.setTheme(android.R.style.Theme_Holo_Dialog);
+                break;
+            case "holo_notitle":
+                this.chclient.setTheme(android.R.style.Theme_Holo_Dialog_NoTitleBar);
+                break;
+            case "holo_minwidth":
+                this.chclient.setTheme(android.R.style.Theme_Holo_Dialog_MinWidth);
                 break;
             case "holo_light":
                 this.chclient.setTheme(android.R.style.Theme_Holo_Light_Dialog);
                 break;
-            case "holo_light_dark":
-                this.chclient.setTheme(android.R.style.Theme_Holo_Light_DarkActionBar_Dialog);
+            case "holo_light_notitle":
+                this.chclient.setTheme(android.R.style.Theme_Holo_Light_Dialog_NoTitleBar);
+                break;
+            case "holo_light_minwidth":
+                this.chclient.setTheme(android.R.style.Theme_Holo_Light_Dialog_MinWidth);
+                break;
+            case "material":
+                this.chclient.setTheme(android.R.style.Theme_Material_Dialog);
+                break;
+            case "material_notitle":
+                this.chclient.setTheme(android.R.style.Theme_Material_Dialog_NoTitleBar);
+                break;
+            case "material_alert":
+                this.chclient.setTheme(android.R.style.Theme_Material_Dialog_Alert);
+                break;
+            case "material_light":
+                this.chclient.setTheme(android.R.style.Theme_Material_Light_Dialog);
+                break;
+            case "material_light_notitle":
+                this.chclient.setTheme(android.R.style.Theme_Material_Light_Dialog_NoTitleBar);
+                break;
+            case "material_light_alert":
+                this.chclient.setTheme(android.R.style.Theme_Material_Light_Dialog_Alert);
+                break;
+            case "devicedef":
+                this.chclient.setTheme(android.R.style.Theme_DeviceDefault_Dialog);
+                break;
+            case "devicedef_alert":
+                //code
+            case "devicedef_light":
+            case "devicedef_light_alert":
+            default:
+                this.chclient.setTheme(android.R.style.Theme_Holo_Light_Dialog);
+                break;
         }
     }
 }
