297fed23cd2ad625960b2e2718cca2724eaa7a8e
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-06-25 08:09:50 -0600

    Add title parameter to ChromeClient and update dialogs

--- app/src/main/java/org/CreadoresProgram/CreaGameBox/engine/ChromeClient.java
+++ app/src/main/java/org/CreadoresProgram/CreaGameBox/engine/ChromeClient.java
@@ -11,11 +11,72 @@ import android.graphics.Color;
 public class ChromeClient extends WebChromeClient{
     private Context context;
     private int theme;
-    public ChromeClient(Context context, int theme){
+    private String title;
+    public ChromeClient(Context context, int theme, String title){
         this.context = context;
+        this.setTitle(title);
         this.setTheme(theme);
     }
     public void setTheme(int theme){
         this.theme = theme;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    @Override
+    public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+        new AlertDialog.Builder(context, theme)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    result.confirm();
+                }
+            })
+            .setCancelable(false)
+            .create().show();
+        return true;
+    }
+    @Override
+    public boolean onJsConfirm(WebView view, String url, String message, JsResult result) {
+        new AlertDialog.Builder(context, theme)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    result.confirm();
+                }
+            })
+            .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    result.cancel();
+                }
+            })
+            .setCancelable(false)
+            .create().show();
+        return true;
+    }
+    @Override
+    public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
+        final EditText input = new EditText(context);
+        input.setText(defaultValue);
+        input.setTextColor(Color.BLACK);//this modify
+        new AlertDialog.Builder(context, theme)
+            .setTitle(title)
+            .setMessage(message)
+            .setView(input)
+            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    result.confirm(input.getText().toString());
+                }
+            })
+            .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    result.cancel();
+                }
+            })
+            .setCancelable(false)
+            .create().show();
+        return true;
     }
 }
