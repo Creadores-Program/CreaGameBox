f2349e9fdfd36b63df973f5c5b6ff951d55838f8
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-07-03 13:53:29 -0600

    Refactor SystemAPIJS class and remove duplicated methods
    Refactored SystemAPIJS to extend SystemAPISandboxJS and removed duplicated methods.

--- app/src/main/java/org/CreadoresProgram/CreaGameBox/system/SystemAPIJS.java
+++ app/src/main/java/org/CreadoresProgram/CreaGameBox/system/SystemAPIJS.java
@@ -27,42 +27,9 @@ import java.io.FileInputStream;
 
 import org.CreadoresProgram.CreaGameBox.MainActivity;
 
-public class SystemAPIJS{
-  private Context context;
+public class SystemAPIJS extends SystemAPISandboxJS {
   public SystemAPIJS(Context context){
-    this.context = context;
-  }
-  @JavascriptInterface
-  public String getManufacturer(){
-    return Build.MANUFACTURER;
-  }
-  @JavascriptInterface
-  public String getModel(){
-    return Build.MODEL;
-  }
-  @JavascriptInterface
-  public String getDevice(){
-    return Build.DEVICE;
-  }
-  @JavascriptInterface
-  public String getProduct(){
-    return Build.PRODUCT;
-  }
-  @JavascriptInterface
-  public String getVersionRelease(){
-    return Build.VERSION.RELEASE;
-  }
-  @JavascriptInterface
-  public int getSdkVersion(){
-    return Build.VERSION.SDK_INT;
-  }
-  @JavascriptInterface
-  public String getLang(){
-    return Locale.getDefault().getDisplayLanguage();
-  }
-  @JavascriptInterface
-  public String getCountry(){
-    return Locale.getDefault().getCountry();
+    super(context);
   }
   @JavascriptInterface
   public void openActivity(String packageName, String activityName){
@@ -83,10 +50,6 @@ public class SystemAPIJS{
   @JavascriptInterface
   public void openApp(String uuid){
     ((MainActivity) context).openApp(uuid);
-  }
-  @JavascriptInterface
-  public void closeApp(){
-    ((MainActivity) context).closeApp();
   }
   @JavascriptInterface
   public void installAppFragment(String uuid, String fileName, String fileBase64){
@@ -236,48 +199,8 @@ public class SystemAPIJS{
     }
   }
   @JavascriptInterface
-  public int getWifiSignalLevel(){
-    try{
-      WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-      if (wifiManager == null || !wifiManager.isWifiEnabled()) {
-        return -1;
-      }
-      WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-      if (wifiInfo == null || wifiInfo.getNetworkId() == -1) {
-        return -1;
-      }
-      int rssi = wifiInfo.getRssi();
-      int totalLevel = 5;
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        return wifiManager.calculateSignalLevel(rssi);
-      }else{
-        return WifiManager.calculateSignalLevel(rssi, totalLevel);
-      }
-    }catch(Exception e){
-      e.printStackTrace();
-      return -1;
-    }
-  }
-  @JavascriptInterface
-  public String getCurrentVersionCGB() {
-    try {
-      PackageInfo pInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
-      return pInfo.versionName;
-    } catch (Exception e) {
-      return "0.0.0";
-    }
-  }
-  @JavascriptInterface
   public void setDeviceIdP1(int id){
     ((MainActivity) this.context).setDeviceIdP1(id);
-  }
-  @JavascriptInterface
-  public int getDeviceIdP1(){
-    return ((MainActivity) this.context).getDeviceIdP1();
-  }
-  @JavascriptInterface
-  public int getCurrentDeviceId(){
-    return ((MainActivity) this.context).getCurrentDeviceId();
   }
   @JavascriptInterface
   public boolean isNoLogin(){
