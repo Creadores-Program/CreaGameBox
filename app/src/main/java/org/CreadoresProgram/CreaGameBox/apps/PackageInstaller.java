0625603641e3d841e31b0d05017fe60cef9f5c7b
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-07-04 22:42:51 -0600

    Add import statement for resource R

--- app/src/main/java/org/CreadoresProgram/CreaGameBox/apps/PackageInstaller.java
+++ app/src/main/java/org/CreadoresProgram/CreaGameBox/apps/PackageInstaller.java
@@ -14,6 +14,7 @@ import java.io.InputStream;
 import java.util.UUID;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import org.CreadoresProgram.CreaGameBox.R;
 
 public class PackageInstaller extends Activity {
 
